cc9ec36d15c3f9293acd80f75349cb88597a1e2e
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-14 01:31:19 +0300

    add

---
+++ 001.StacksAndQueuesLab/007.HotPotato/HotPotato.cs
@@ -0,0 +1,23 @@
+﻿//Stack and queues - lab, Task 007
+//14.05.2025, 01:06
+
+var players = Console.ReadLine().Split().ToArray();
+int n = int.Parse(Console.ReadLine());
+
+Queue<string> names = new Queue<string>(players);
+
+while(names.Count > 1)
+{
+    for(int i = 1; i < n; i++)
+    {
+        string player = names.Dequeue();
+        names.Enqueue(player);
+    }
+
+    string removedPLayer = names.Dequeue();
+    Console.WriteLine($"Removed {removedPLayer}");
+}
+
+string lastPlayer = names.Dequeue();
+Console.WriteLine($"Last is {lastPlayer}");
+
