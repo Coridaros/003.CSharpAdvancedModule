9de3a63dcb1b2a83b87a684ec8eccbc855eccada
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-14 00:46:52 +0300

    add

---
+++ 001.StacksAndQueuesLab/004.MatchingBrackets/MatchingBrackets.cs
@@ -0,0 +1,23 @@
+﻿//Stacks and queues - lab, Task 004
+//13.05.2025, 23:41
+
+string str = Console.ReadLine();
+
+Stack<int> stack = new Stack<int>();
+
+for(int i = 0; i < str.Length; i++)
+{
+    if (str[i] == '(')
+    {
+        stack.Push(i);
+    }
+    else if (str[i] == ')')
+    {
+        int startIndex = stack.Pop();
+        int endIndex = i;
+
+        string result = str.Substring(startIndex, endIndex - startIndex + 1);
+
+        Console.WriteLine(result);
+    }
+}
