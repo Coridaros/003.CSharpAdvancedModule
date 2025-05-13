522dabc653c855583ce9263f3875b91cbf97b11c
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-14 01:31:42 +0300

    add

---
+++ 001.StacksAndQueuesLab/008.TrafficJam/TrafficJam.cs
@@ -0,0 +1,34 @@
+﻿//Stacks and queues - lab, Task 008
+//14.05.2025, 01:17
+
+int n = int.Parse(Console.ReadLine());
+
+int carPassedCnt = 0;
+
+Queue<string> cars = new Queue<string>();
+
+while(true)
+{
+    string command = Console.ReadLine();
+
+    if(command == "green")
+    {
+        for(int i = 0; i < n; i++)
+        {
+            if(cars.Count > 0)
+            {
+                Console.WriteLine($"{cars.Dequeue()} passed!");
+                carPassedCnt++;
+            }
+        }
+    }
+    else if(command == "end")
+    {
+        Console.WriteLine($"{carPassedCnt} cars passed the crossroads.");
+        break;
+    }
+    else
+    {
+        cars.Enqueue(command);
+    }
+}
