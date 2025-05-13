de7a8c13ebda7c0290510bb5fc0a7832d4835a40
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-14 00:46:11 +0300

    add

---
+++ 001.StacksAndQueuesLab/002.StackSum/StackSum.cs
@@ -0,0 +1,38 @@
+﻿//Stack and queues - lab, Task 002
+//13.05.2025, 22:49
+
+var inputs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Stack<int> stack = new Stack<int>(inputs);
+
+while(true)
+{
+    string command = Console.ReadLine().ToLower();
+    string[] tokens = command.Split().ToArray();
+
+    if (tokens[0] == "add")
+    {
+        int firstN = int.Parse(tokens[1]);
+        int secondN = int.Parse(tokens[2]);
+
+        stack.Push(firstN);
+        stack.Push(secondN);
+    }
+    else if (tokens[0] == "remove")
+    {
+        int count = int.Parse(tokens[1]);
+
+        if(count <= stack.Count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                stack.Pop();
+            }
+        }
+    }
+    else if (command == "end")
+    {
+        Console.WriteLine($"Sum: {stack.Sum()}");
+        break;
+    }
+}
