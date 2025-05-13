7392d1b8351e9b97ba9aceb81eb3486f1075b09a
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-14 00:45:48 +0300

    add

---
+++ 001.StacksAndQueuesLab/001.ReverseAString/ReverseAString.cs
@@ -0,0 +1,16 @@
+﻿//Stacks and queues - lab, Task 001
+//13.05.2025, 21:31
+
+string input = Console.ReadLine();
+
+Stack<char> stack = new Stack<char>();
+
+for(int i = 0; i < input.Length; i++)
+{
+    stack.Push(input[i]);
+}
+
+while(stack.Count > 0)
+{
+    Console.Write(stack.Pop());
+}
