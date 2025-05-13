147d2e2c1dfc447d3d3eb004c0e4e5ced418f2bf
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-14 01:30:56 +0300

    add

---
+++ 001.StacksAndQueuesLab/006.Supermarket/Supermarket.cs
@@ -0,0 +1,27 @@
+﻿//Stacks and queues - lab, Task 006
+//14.05.2025, 00:57
+
+Queue<string> market = new Queue<string>();
+
+while(true)
+{
+    string text = Console.ReadLine();
+
+    if(text == "Paid")
+    {
+        while(market.Count > 0)
+        {
+            Console.WriteLine(market.Dequeue());
+        }
+    }
+    else if(text == "End")
+    {
+        int rCnt = market.Count;
+        Console.WriteLine($"{rCnt} people remaining.");
+        break;
+    }
+    else
+    {
+        market.Enqueue(text);
+    }
+}
