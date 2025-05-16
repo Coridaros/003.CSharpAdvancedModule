68e735d9712dfce186aef430013953c438e651e7
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-16 23:24:44 +0300

    add

---
+++ 002.StacksAndQueuesExercise/004.FastFood/FastFood.cs
@@ -0,0 +1,37 @@
+﻿//Stacs and queues - exercise, Task 004
+//16.05.2025, 01:35
+
+int food = int.Parse(Console.ReadLine());
+
+
+
+var queue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+int queCnt = queue.Count;
+int max = queue.Max();
+
+for (int i = 1; i <= queCnt; i++)
+{
+    int curElement = queue.Peek();
+
+    if(food >= curElement)
+    {
+        food -= curElement;
+        queue.Dequeue();
+    }
+    else
+    {
+        break;
+    }
+}
+
+Console.WriteLine(max);
+
+if(queue.Count == 0)
+{
+    Console.WriteLine("Orders complete");
+}
+else
+{
+    Console.WriteLine($"Orders left: {string.Join(" " , queue)}");
+}
