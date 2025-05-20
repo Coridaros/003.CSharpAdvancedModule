dbcb83132fc34f78690d0fbd70ba015012b45f4c
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-21 00:57:27 +0300

    add

---
+++ 002.StacksAndQueuesExercise/007.TruckTour/TruckTour.cs
@@ -0,0 +1,24 @@
+﻿//Stacks and queues - exercise, Task 007
+//20.05.2025, 21:48
+
+int n = int.Parse(Console.ReadLine());
+
+int startIndex = 0;
+int currentPetrol = 0;
+
+for(int i = 0; i < n; i++)
+{
+    int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    int fuel = data[0];
+    int distance = data[1];
+
+    currentPetrol += fuel - distance;
+
+    if(currentPetrol < 0)
+    {
+        startIndex = i + 1;
+        currentPetrol = 0;
+    }
+}
+
+Console.WriteLine(startIndex);
