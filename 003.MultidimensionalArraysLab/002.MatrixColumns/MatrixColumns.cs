9f3d512c5fb6c6a3200e78b7dc0f3606cd35582f
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-23 02:17:27 +0300

    add

---
+++ 003.MultidimensionalArraysLab/002.MatrixColumns/MatrixColumns.cs
@@ -0,0 +1,28 @@
+﻿//Multidimensional arrays - lab, Task 002
+//22.05.2025, 22:56
+
+int[] dimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+int[,] matrix = new int[dimensions[0], dimensions[1]];
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = numbers[j];
+    }
+}
+
+for(int i = 0; i < matrix.GetLength(1); i++)
+{
+    int sum = 0;
+
+    for(int j = 0; j < matrix.GetLength(0); j++)
+    {
+        sum += matrix[j, i];
+    }
+
+    Console.WriteLine(sum);
+}
