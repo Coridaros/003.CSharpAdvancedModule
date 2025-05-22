8586367bf82bb76230604b0a2fed7af02bc9173f
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-23 02:18:04 +0300

    add

---
+++ 003.MultidimensionalArraysLab/004.SymbolInMatrix/SymbolInMatrix.cs
@@ -0,0 +1,35 @@
+﻿//Multidimensional arrays - lab, Task 004
+//22.05.2025, 23:22
+
+int size = int.Parse(Console.ReadLine());
+
+char[,] matrix = new char[size, size];
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    char[] symbols = Console.ReadLine().ToCharArray();
+
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = symbols[j];
+    }
+}
+
+char symbol = char.Parse(Console.ReadLine());
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if(matrix[i, j] == symbol)
+        {
+            Console.WriteLine($"({i}, {j})");
+            return;
+        }
+    }
+}
+
+Console.WriteLine($"{symbol} does not occur in the matrix");
+
+
+
