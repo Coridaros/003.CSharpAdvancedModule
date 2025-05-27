09e2174e313b24ea1143dcfd66d971defd98d5ce
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-27 23:19:00 +0300

    add

---
+++ 005.SetAndDictionariesAdvLab/002.AverageStudentGrades/AverageStudentGrades.cs
@@ -0,0 +1,48 @@
+﻿//Set and dictionaries advanced - lab, Task 002
+//27.05.2025, 00:15
+
+public class AverageStudentGrades
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Dictionary<string, List<decimal>> studentInfo = new Dictionary<string, List<decimal>>();
+
+        for(int i = 0; i < n; i++)
+        {
+            string[] inputData = Console.ReadLine().Split();
+            string studentName = inputData[0];
+            decimal grade = decimal.Parse(inputData[1]);
+
+            //ако студента не съществува в речника го добавяме и създаваме нов списък с оценки към него
+            if(!studentInfo.ContainsKey(studentName))
+            {
+                studentInfo[studentName] = new List<decimal>();
+            }
+
+            //добавяме към списъка на студента оценки
+            studentInfo[studentName].Add(grade);
+        }
+
+        PrintStudentGrades(studentInfo);
+    }
+
+    static void PrintStudentGrades(Dictionary<string, List<decimal>> studentsInfo)
+    {
+        foreach(var item in studentsInfo)
+        {
+            var grades = item.Value;
+            decimal avg = grades.Average();
+
+            Console.Write($"{item.Key} -> ");
+
+            foreach(var grade in grades)
+            {
+                Console.Write($"{grade:F2} ");
+            }
+
+            Console.WriteLine($"(avg: {avg:F2})");
+        }
+    }
+}
