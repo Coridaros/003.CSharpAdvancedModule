a8a45726c1aa9742c589313b34bba8976c894944
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-27 23:19:27 +0300

    add

---
+++ 005.SetAndDictionariesAdvLab/003.LargestThreeNumbers/LargestThreeNumbers.cs
@@ -0,0 +1,12 @@
+﻿//Set and dictionaries advanced - lab, Task 003
+//27.05.2025, 00:55
+
+public class LargestThreeNumbers
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(n => n).Take(3).ToArray();
+
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+}
