9ea15f76c24e6047c8257af2bff4b87299e2abb7
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-02 23:26:29 +0300

    add

---
+++ 006.SetsAndDictionaresExercise/003.PeriodicTable/PeriodicTable.cs
@@ -0,0 +1,24 @@
+﻿//Sets and dictionaries - exercise, Task 003
+//02.06.2025, 23:05
+
+public class PeriodicTable
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        SortedSet<string> elements = new SortedSet<string>();
+        
+        for(int i = 0; i < n; i++)
+        {
+            string[] currElement = Console.ReadLine().Split(" ");
+
+            foreach(string element in currElement)
+            {
+                elements.Add(element);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", elements));
+    }
+}
