5e06be678882b324a49ea7175ff893cc84925373
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-02 23:24:04 +0300

    add

---
+++ 005.SetAndDictionariesAdvLab/006.RecordUniqueNames/RecordUniqueNames.cs
@@ -0,0 +1,22 @@
+﻿//Sets and dictionaries advanced - lab, Task 006
+//28.05.2025, 01:05
+
+public class RecordUniqueNames
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        HashSet<string> names = new HashSet<string>();
+
+        for(int i = 0; i < n; i++)
+        {
+            names.Add(Console.ReadLine());
+        }
+
+        foreach(var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
