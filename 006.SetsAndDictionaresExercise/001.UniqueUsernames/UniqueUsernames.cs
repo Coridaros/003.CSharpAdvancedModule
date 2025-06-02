523dcfe4bfcde23640f17c70bee7c9f17a14926a
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-02 23:25:46 +0300

    add

---
+++ 006.SetsAndDictionaresExercise/001.UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,22 @@
+﻿//Sets and dictionaries - exercise, Task 001
+//02.06.2025, 22:49
+
+public class UniqueUsernames
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        HashSet<string> usernames = new HashSet<string>();
+
+        for(int i = 0; i < n; i++)
+        {
+            usernames.Add(Console.ReadLine());
+        }
+
+        foreach(string username in usernames)
+        {
+            Console.WriteLine(username);
+        }
+    }
+}
