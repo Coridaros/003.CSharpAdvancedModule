123a30ec527c284b6e2d69f9b89bb38491cb3026
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-06 23:37:22 +0300

    add

---
+++ 007.StreamsFilesAndDirectoriesLab/000.Example/Example.cs
@@ -0,0 +1,39 @@
+﻿//Streams, files and directories - lab, Exaple
+//04.06.2025, 22:50
+
+public class Example
+{
+    static void Main()
+    {
+        string inputFile = "../../../input.txt";
+
+        StreamReader reader = new StreamReader(inputFile);
+
+        ConPlay();
+
+        using(reader)
+        {
+            int cnt = 1;
+
+            while(true)
+            {
+                string line = reader.ReadLine();
+
+                if(line == null)
+                {
+                    break;
+                }
+
+                Console.WriteLine($"{cnt}. {line}");
+
+                cnt++;
+            }
+        }
+
+        static void ConPlay()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Title = "Example";
+        }
+    }
+}
