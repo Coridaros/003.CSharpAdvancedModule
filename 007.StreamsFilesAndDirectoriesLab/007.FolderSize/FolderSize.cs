11f4d74c9fc1f6b1a04fb0df2e5be7238db0b0d0
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-08 23:08:10 +0300

    add

---
+++ 007.StreamsFilesAndDirectoriesLab/007.FolderSize/FolderSize.cs
@@ -0,0 +1,36 @@
+﻿//Streams, files and directories - lab, Task 007
+//07.06.2025, 02:31
+
+namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main()
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            double sum = 0;
+
+            var directory = new DirectoryInfo(folderPath);
+            FileInfo[] files = directory.GetFiles("*", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                sum += file.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+
+            using (var streamWriter = new StreamWriter(outputFilePath))
+            {
+                streamWriter.WriteLine(sum.ToString());
+            }
+        }
+    }
+}
