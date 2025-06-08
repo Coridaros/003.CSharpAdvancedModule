56f36885a16d72032e2af71ceb249af0a8eb751f
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-08 23:00:51 +0300

    add

---
+++ 007.StreamsFilesAndDirectoriesLab/005.ExtractSpecialBytes/ExtractSpecialBytes.cs
@@ -0,0 +1,28 @@
+﻿//Streams, files and directories - lab, Task 005
+//06.06.2025, 23:46
+
+namespace ExtractSpecialBytes;
+
+public class ExtractSpecialBytes
+{
+    static void Main()
+    {
+        string binaryFilePath = @"..\..\..\Files\example.png";
+        string bytesFilePath = @"..\..\..\Files\bytes.txt";
+        string outputPath = @"..\..\..\Files\output.bin";
+
+        ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+
+    }
+
+    public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+    {
+        HashSet<byte> targetB = new HashSet<byte>(File.ReadAllLines(bytesFilePath)
+            .Select(l => byte.Parse(l.Trim())));
+
+        byte[] input = File.ReadAllBytes(binaryFilePath);
+        byte[] filteredData = input.Where(b => targetB.Contains(b)).ToArray();
+
+        File.WriteAllBytes(outputPath, filteredData);
+    }
+}
