5d9c3052cf0fdb2f9266c8d750ebc44b096d1f6c
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-11 00:54:09 +0300

    add

---
+++ 009.FunctionalProgrammingLab/004.AddVAT/AddVAT.cs
@@ -0,0 +1,16 @@
+﻿//Functional programming - lab, Task 004
+//10.06.2025, 01:31
+
+public class AddVAT
+{
+    static void Main()
+    {
+        double[] prices = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse)
+            .Select(x => x * 1.20).ToArray();
+
+        foreach(var item in prices)
+        {
+            Console.WriteLine($"{item:F2}");
+        }
+    }
+}
