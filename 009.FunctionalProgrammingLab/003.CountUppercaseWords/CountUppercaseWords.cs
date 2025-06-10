dbe04dac1d15cbf059c62aada7cd9365ae7a4440
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-11 00:53:50 +0300

    add

---
+++ 009.FunctionalProgrammingLab/003.CountUppercaseWords/CountUppercaseWords.cs
@@ -0,0 +1,17 @@
+﻿//Functional programming - lab, Task 003
+//10.06.2025, 01:23
+
+public class CountUppercaseWords
+{
+    static void Main()
+    {
+        Predicate<string> checker = n => n[0] == n.ToUpper()[0];
+
+        string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(x => checker(x)).ToArray();
+
+        foreach(string word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
