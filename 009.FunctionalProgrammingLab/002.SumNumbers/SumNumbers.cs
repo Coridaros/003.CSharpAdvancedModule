115bc3db91a592feecc7f23f9c741618c941ac06
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-11 00:53:29 +0300

    add

---
+++ 009.FunctionalProgrammingLab/002.SumNumbers/SumNumbers.cs
@@ -0,0 +1,15 @@
+﻿//Functional programming - lab, Task 002
+//10.06.2025, 01:18
+
+public class SumNumbers
+{
+    static void Main()
+    {
+        Func<string, int> parser = x => int.Parse(x);
+
+        int[] numbers = Console.ReadLine().Split(", ").Select(parser).ToArray();
+
+        Console.WriteLine(numbers.Count());
+        Console.WriteLine(numbers.Sum());
+    }
+}
