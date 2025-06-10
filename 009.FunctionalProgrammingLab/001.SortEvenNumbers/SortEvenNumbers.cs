44efb58f2720ea635b12c830e8759ce1a0dc8ba2
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-11 00:53:09 +0300

    add

---
+++ 009.FunctionalProgrammingLab/001.SortEvenNumbers/SortEvenNumbers.cs
@@ -0,0 +1,12 @@
+﻿//Functional programming - lab, Task 001
+//10.06.2025, 01:11
+
+public class SortEvenNumbers
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine().Split(", ").Select(x => int.Parse(x)).Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+
+        Console.WriteLine(string.Join(", ", numbers));
+    }
+}
