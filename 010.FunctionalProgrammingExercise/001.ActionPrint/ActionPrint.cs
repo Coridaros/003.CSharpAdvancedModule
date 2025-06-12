d5902c2ba2d14ec4c16f7f5db4dbe951caf606e4
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-12 21:25:39 +0300

    add

---
+++ 010.FunctionalProgrammingExercise/001.ActionPrint/ActionPrint.cs
@@ -0,0 +1,14 @@
+﻿//Fuctional programming - exercise, Task 001
+//10.06.2025, 11:01
+
+public class ActionPrint
+{
+    static void Main()
+    {
+        List<string> names = Console.ReadLine().Split().ToList();
+
+        Action<string> printName = name => Console.WriteLine(name);
+
+        names.ForEach(printName);
+    }
+}
