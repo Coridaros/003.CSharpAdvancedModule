81d6c9b4c81fc77b28e585b94176e58555dffdad
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-12 21:27:13 +0300

    add

---
+++ 010.FunctionalProgrammingExercise/006.ReverseAndExclude/ReverseAndExclude.cs
@@ -0,0 +1,19 @@
+﻿//Functional programming - exercise, Task 006
+//10.06.2025, 19:34
+
+public class ReverseAndExclude
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+        int n = int.Parse(Console.ReadLine());
+
+        Action<List<int>> removeAll = list => list.RemoveAll(x => x % n == 0);
+
+        removeAll(numbers);
+
+        numbers.Reverse();
+
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+}
