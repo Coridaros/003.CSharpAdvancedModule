d8ad6851769cd2312ca6891bd2e9bd6aff17a760
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-12 21:28:48 +0300

    add

---
+++ 010.FunctionalProgrammingExercise/011.TriFunction/TriFunction.cs
@@ -0,0 +1,19 @@
+﻿//Functional programming - exercise, Task 011
+//12.06.2025, 21:22
+
+public class TriFunction
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        var names = Console.ReadLine().Split().ToArray();
+
+        Func<string, int, bool> checkName = (name, sum) => name.Sum(c => c) >= sum;
+
+        Func<string[], int, Func<string, int, bool>, string> getName = (names, sum, firstName) =>
+        names.FirstOrDefault(name => firstName(name, sum));
+
+        Console.WriteLine(getName(names, number, checkName));
+    }
+}
