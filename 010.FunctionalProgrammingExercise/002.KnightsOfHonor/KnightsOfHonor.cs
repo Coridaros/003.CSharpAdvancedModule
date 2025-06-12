b8d73f9d292754a2d69a1c3d0dda87674541c46b
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-12 21:25:58 +0300

    add

---
+++ 010.FunctionalProgrammingExercise/002.KnightsOfHonor/KnightsOfHonor.cs
@@ -0,0 +1,17 @@
+﻿//Functional programming - exercise, Task 002
+//10.06.2025, 11:05
+
+public class KnightsOfHonor
+{
+    static void Main()
+    {
+        List<string> names = Console.ReadLine().Split().ToList();
+
+        Action<string> title = name => Console.WriteLine($"Sir {name}");
+
+        foreach(string name in names)
+        {
+            title(name);
+        }
+    }
+}
