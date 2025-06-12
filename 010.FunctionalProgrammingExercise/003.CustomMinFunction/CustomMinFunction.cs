ae01a59f4bc48463a6c28136202ea89d0c976c42
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-12 21:26:16 +0300

    add

---
+++ 010.FunctionalProgrammingExercise/003.CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,14 @@
+﻿//Functional programming - exercise, Task 003
+//10.06.2025, 11:09
+
+public class CustomMinFunction
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        Func<List<int>, int> minNumber = nums => nums.Min();
+
+        Console.WriteLine(minNumber(numbers));
+    }
+}
