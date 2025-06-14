fe510c0d9a96fca66f0571035189478a37acbe09
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-14 23:13:48 +0300

    add

---
+++ 011.DefiningClasses/001.Car/Car.cs
@@ -0,0 +1,47 @@
+﻿//Defining classes - lab, Task 001
+//12.06.2025, 23:24
+
+namespace CarManufacturer;
+
+public class Car
+{
+    private string make;
+    private string model;
+    private int year;
+
+    public string Make
+    {
+        get
+        { 
+            return make; 
+        }
+        set
+        {
+            make = value;
+        }
+    }
+
+    public string Model
+    {
+        get
+        {
+            return model;
+        }
+        set
+        {
+            model = value;
+        }
+    }
+
+    public int Year
+    {
+        get
+        {
+            return year;
+        }
+        set
+        {
+            year = value;
+        }
+    }
+}
---
+++ 011.DefiningClasses/001.Car/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Car golf = new Car();
+
+        golf.Make = "VW";
+        golf.Model = "Golf 5";
+        golf.Year = 2004;
+
+        Console.WriteLine($"Make: {golf.Make} \r\nModel: {golf.Model} \r\nYear: {golf.Year}");
+    }
+}
