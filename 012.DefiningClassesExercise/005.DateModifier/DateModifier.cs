381ec64efc634cab6e7ae7007599dfc17251179f
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-17 01:06:37 +0300

    add

---
+++ 012.DefiningClassesExercise/005.DateModifier/DateModifier.cs
@@ -0,0 +1,34 @@
+﻿//Defining classes - exercise, Task 005
+//15.06.2025, 00:46
+
+using System.Globalization;
+
+namespace DateModifier;
+
+public class DateModifier
+{
+    //private int days;
+
+    //public int Days { get; set; }
+
+    public static int CalculateDiff(string dateOne, string dateTwo)
+    {
+        DateTime firstDate = DateTime.ParseExact(dateOne, "yyyy MM dd", CultureInfo.InvariantCulture);
+        DateTime secondDate = DateTime.ParseExact(dateTwo, "yyyy MM dd", CultureInfo.InvariantCulture);
+
+        int diff = Math.Abs((firstDate - secondDate).Days);
+
+        return diff;
+    }
+}
+
+public class Test
+{
+    static void Main()
+    {
+        string dateOne = Console.ReadLine();
+        string dateTwo = Console.ReadLine();
+
+        Console.WriteLine(DateModifier.CalculateDiff(dateOne, dateTwo));
+    }
+}
