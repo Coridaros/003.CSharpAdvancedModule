221f115ab31971bffca017c721288ad392113ab2
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-17 01:05:18 +0300

    add

---
+++ 012.DefiningClassesExercise/002.CreatingConstructors/StartUp.cs
@@ -0,0 +1,20 @@
+﻿//Defining classes - exercise, Task 002
+//14.06.2025, 23:36
+
+namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Person firstPerson = new Person();
+
+        Console.WriteLine($"{firstPerson.Name} - {firstPerson.Age}");
+
+        Person secondPerson = new Person(20);
+
+        Person thirdPerson = new Person("Pesho", 22);
+
+        Console.WriteLine($"{thirdPerson.Name} - {thirdPerson.Age}");
+    }
+}
