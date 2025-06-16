372639eb44c44b73e29b1ff96e094607a1669f24
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-17 01:06:14 +0300

    add

---
+++ 012.DefiningClassesExercise/004.OpinionPoll/StartUp.cs
@@ -0,0 +1,34 @@
+﻿//Difining classes - exercise, Task 004
+//15.06.2025, 00:35
+
+using System;
+
+namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Family family = new Family();
+
+        for(int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            string name = input[0];
+            int age = int.Parse(input[1]);
+
+            Person person = new Person(name, age);
+
+            family.AddMember(person);
+        }
+
+        List<Person> p = family.People.Where(pp => pp.Age > 30).ToList();
+
+        foreach(var item in p.OrderBy(x => x.Name))
+        {
+            Console.WriteLine($"{item.Name} - {item.Age}");
+        }
+    }
+}
