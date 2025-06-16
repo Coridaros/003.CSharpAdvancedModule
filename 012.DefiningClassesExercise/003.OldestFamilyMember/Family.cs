fcd7fbf31b3b23c770ea4c9d5791fceb834ee6e1
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-17 01:05:49 +0300

    add

---
+++ 012.DefiningClassesExercise/003.OldestFamilyMember/Family.cs
@@ -0,0 +1,25 @@
+﻿namespace DefiningClasses;
+
+public class Family
+{
+    private List<Person> people;
+
+    public List<Person> People { get; set; }
+
+    public Family()
+    {
+        this.People = new List<Person>();
+    }
+
+    public void AddMember(Person member)
+    {
+        this.People.Add(member);
+    }
+
+    public Person GetOldestMember()
+    {
+        Person person = People.OrderByDescending(p => p.Age).First();
+
+        return person;
+    }
+}
---
+++ 012.DefiningClassesExercise/003.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,31 @@
+﻿//Defining classes - exercise, Task 003
+//15.06.2025, 00:01
+
+using System;
+
+namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Family family = new Family();
+
+        for(int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            string name = input[0];
+            int age = int.Parse(input[1]);
+
+            Person person = new Person(name, age);
+
+            family.AddMember(person);
+        }
+
+        Person oldest = family.GetOldestMember();
+
+        Console.WriteLine($"{oldest.Name} {oldest.Age}");
+    }
+}
