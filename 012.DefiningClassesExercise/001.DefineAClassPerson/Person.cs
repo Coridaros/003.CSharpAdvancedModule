b2103e31407216c97dbf2ac80e1a2127d9c713a2
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-17 01:04:48 +0300

    add

---
+++ 012.DefiningClassesExercise/001.DefineAClassPerson/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace DefiningClasses;
+
+public class Person
+{
+    private string name = null;
+    private int age;
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
---
+++ 012.DefiningClassesExercise/001.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,12 @@
+﻿//Defining classes - exercise, Task 001
+//14.06.2025, 23:29
+
+namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main()
+    {
+
+    }
+}
