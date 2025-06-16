bcfe2ab7d555b34591a18fa35e1e943a3aab0adc
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-17 01:07:08 +0300

    add

---
+++ 012.DefiningClassesExercise/006.SpeedRacing/Car.cs
@@ -0,0 +1,35 @@
+﻿public class Car
+{
+    private string model;
+    private double fuelAmount;
+    private double fuelConsumptionPerKilometer;
+    private double travelleddistance;
+
+    public string Model { get; set; }
+    public double FuelAmount { get; set; }
+    public double FuelConsumptionPerKilometer;
+    public double Travelleddistance { get; set; } // тук може да даде грешка...
+
+    public Car(string model, double fuelAmount, double consumption)
+    {
+        this.Model = model;
+        this.FuelAmount = fuelAmount;
+        this.FuelConsumptionPerKilometer = consumption;
+        this.Travelleddistance = 0.0;
+    }
+
+    public void Drive(string carModel, double kilometers)
+    {
+        bool canContinue = this.FuelAmount - (kilometers * (this.FuelConsumptionPerKilometer)) >= 0;
+
+        if (canContinue)
+        {
+            this.FuelAmount -= kilometers * (this.FuelConsumptionPerKilometer);
+            this.Travelleddistance += kilometers;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient fuel for the drive");
+        }
+    }
+}
---
+++ 012.DefiningClassesExercise/006.SpeedRacing/StartUp.cs
@@ -0,0 +1,51 @@
+﻿//Defining classes - exercise, Task 006
+//15.06.2025, 01:02
+
+
+public class StartUp
+{
+    static void Main()
+    {
+        List<Car> cars = new List<Car>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            string model = input[0];
+            double fuel = double.Parse(input[1]);
+            double fuelPerKm = double.Parse(input[2]);
+
+            Car car = new Car(model, fuel, fuelPerKm);
+
+            cars.Add(car);
+        }
+
+        while(true)
+        {
+            string command = Console.ReadLine();
+
+            if(command == "End")
+            {
+                break;
+            }
+
+            string model = command.Split()[1];
+            double distance = double.Parse(command.Split()[2]);
+
+            foreach(var item in cars)
+            {
+                if(item.Model == model)
+                {
+                    item.Drive(model, distance);
+                }
+            }
+        }
+
+        foreach(var item in cars)
+        {
+            Console.WriteLine($"{item.Model} {item.FuelAmount:F2} {item.Travelleddistance}");
+        }
+    }
+}
