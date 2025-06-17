cad6ba917dfefd8b0d25002acaf9d24cec98feff
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-18 00:45:17 +0300

    rdy

--- 012.DefiningClassesExercise/008.CarSalesman/Car.cs
+++ 012.DefiningClassesExercise/008.CarSalesman/Car.cs
@@ -1,23 +1,57 @@
-﻿public class Car
+﻿using System.Text;
+
+public class Car
 {
-    private string model;
-    private Engine engine;
-    private int weight;
-    private string color;
-
-    public string Model { get; set; }
-    public Engine Engine { get; set; }
-    public int Weigth { get; set; }
-    public string Color { get; set; }
-
     public Car(string model, Engine engine)
     {
         this.Model = model;
         this.Engine = engine;
     }
+    public Car(string model, Engine engine, string color) : this(model, engine)
+    {
+        this.Color = color;
+    }
+    public Car(string model, Engine engine, int weight) : this(model, engine)
+    {
+        this.Weight = weight;
+    }
+
+    public Car(string model, Engine engine, int weight, string color) : this(model, engine, weight)
+    {
+        this.Color = color;
+    }
+
+    public string Model { get; set; }
+    public Engine Engine { get; set; }
+    public int Weight { get; set; } = 0;
+    public string Color { get; set; } = "n/a";
 
     public override string ToString()
     {
-        return base.ToString();
+        var stringBuilder = new StringBuilder();
+
+        stringBuilder.AppendLine($"{Model}:");
+        stringBuilder.AppendLine($"  {Engine.Model}:");
+        stringBuilder.AppendLine($"    Power: {Engine.Power}");
+        if (Engine.Displacement != 0)
+        {
+            stringBuilder.AppendLine($"    Displacement: {Engine.Displacement}");
+        }
+        else
+        {
+            stringBuilder.AppendLine($"    Displacement: n/a");
+        }
+        stringBuilder.AppendLine($"    Efficiency: {Engine.Efficiency}");
+        if (Weight != 0)
+        {
+            stringBuilder.AppendLine($"  Weight: {Weight}");
+        }
+        else
+        {
+            stringBuilder.AppendLine($"  Weight: n/a");
+        }
+        stringBuilder.AppendLine($"  Color: {Color}");
+
+        return stringBuilder.ToString();
     }
 }
