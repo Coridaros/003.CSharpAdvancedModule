3fd475d7d88bb629528cd69ea2b330f722c35d95
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-18 00:45:47 +0300

    add

---
+++ 012.DefiningClassesExercise/009.PokemonTrainer/Trainer.cs
@@ -0,0 +1,17 @@
+﻿public class Trainer
+{
+    public Trainer(string name)
+    {
+        this.Name = name;
+        this.Pokemons = new List<Pokemon>();
+    }
+
+    public string Name { get; set; }
+    public int NumberOfBadges { get; set; } = 0;
+    public List<Pokemon> Pokemons { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} {NumberOfBadges} {Pokemons.Count}";
+    }
+}
