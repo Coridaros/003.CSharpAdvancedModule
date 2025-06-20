478495c53261dd620cfd699738da02c3a9d36d03
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-21 00:11:23 +0300

    add

--- 015.GenericsLab/001.Box/StartUp.cs
+++ 015.GenericsLab/001.Box/StartUp.cs
@@ -1,7 +1,23 @@
 ﻿//Generics - lab, Task 001
 //19.06.2025, 15:12
 
+namespace BoxOfT;
+
 public class StartUp
 {
+    static void Main()
+    {
+        Box<int> box = new Box<int>();
 
+        box.Add(1);
+        box.Add(2);
+        box.Add(3);
+
+        Console.WriteLine(box.Remove());
+
+        box.Add(4);
+        box.Add(5);
+
+        Console.WriteLine(box.Remove());
+    }
 }
