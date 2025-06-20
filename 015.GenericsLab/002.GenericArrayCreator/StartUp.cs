aa480dc098fda82cb95b28eb280ef98abfe15013
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-21 00:11:50 +0300

    add

---
+++ 015.GenericsLab/002.GenericArrayCreator/StartUp.cs
@@ -0,0 +1,13 @@
+﻿//Generics - lab, Task 002
+//21.06.2025, 00:06
+
+namespace GenericArrayCreator;
+
+public class StartUp
+{
+    static void Main()
+    {
+        string[] stringArray = ArrayCreator.Create(5, "Pesho");
+        int[] intArray = ArrayCreator.Create(10, 33);
+    }
+}
