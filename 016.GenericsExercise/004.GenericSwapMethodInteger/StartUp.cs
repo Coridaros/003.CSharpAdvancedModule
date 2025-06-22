f8c4538e445c37dd9c1af64f03f006bc981764cb
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-22 19:40:03 +0300

    add

---
+++ 016.GenericsExercise/004.GenericSwapMethodInteger/StartUp.cs
@@ -0,0 +1,29 @@
+﻿//Generics - exercise, Task 004
+//20.06.2025, 00:47
+
+namespace GenericSwapMethodInteger;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Box<int> box = new Box<int>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            box.Add(int.Parse(Console.ReadLine()));
+        }
+
+        int[] swapIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int firstIndex = swapIndexes[0];
+        int secondIndex = swapIndexes[1];
+
+        var list = box.ToList();
+        box.Swap(list, firstIndex, secondIndex);
+
+        Console.WriteLine(box.ToString());
+    }
+}
