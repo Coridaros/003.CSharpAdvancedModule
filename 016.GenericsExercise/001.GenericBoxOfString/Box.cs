9ae9820b31fc2f99e8ba254c90e174983c431ecc
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-22 19:39:04 +0300

    add

---
+++ 016.GenericsExercise/001.GenericBoxOfString/Box.cs
@@ -0,0 +1,26 @@
+﻿namespace GenericBoxOfString;
+
+public class Box<T>
+{
+    private List<T> box;
+
+    public Box()
+    {
+        this.box = new List<T>();
+    }
+
+    public void Add(T element)
+    {
+        this.box.Add(element);
+    }
+
+    public void Clear()
+    {
+        this.box.Clear();
+    }
+
+    public override string ToString()
+    {
+        return $"{typeof(T)}: {string.Join(" ", box)}";
+    }
+}
---
+++ 016.GenericsExercise/001.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,24 @@
+﻿//Generics - exercise, Task 001
+//19.06.2025, 15:19
+
+namespace GenericBoxOfString;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Box<string> box = new Box<string>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+
+            box.Add(input);
+
+            Console.WriteLine(box.ToString());
+            box.Clear();
+        }
+    }
+}
