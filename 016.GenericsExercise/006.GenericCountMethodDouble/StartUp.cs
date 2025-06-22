d1dd1b424e454ce128fa64a40a8a44a1a84eadab
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-22 19:40:43 +0300

    add

---
+++ 016.GenericsExercise/006.GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,39 @@
+﻿//Generics - exercise, Task 006
+//20.06.2025, 00:53
+
+namespace GenericCountMethodString;
+
+public class StartUp
+{
+    static void Main()
+    {
+        var boxes = new List<Box<double>>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Box<double> box = new Box<double>(double.Parse(Console.ReadLine()));
+
+            boxes.Add(box);
+        }
+
+        double compare = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(Count(boxes, compare));
+    }
+
+    public static int Count<T>(List<Box<T>> list, T element) where T : IComparable<T>
+    {
+        int count = 0;
+
+        foreach (var item in list)
+        {
+            if (item.CompareTo(element) > 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
