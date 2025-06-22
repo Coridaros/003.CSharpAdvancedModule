9f5155661ac7b7e0aedf3f000ea7d7058ab53c1d
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-22 19:41:02 +0300

    add

---
+++ 016.GenericsExercise/007.Tuple/StartUp.cs
@@ -0,0 +1,34 @@
+﻿//Generics - exercise, Task 007
+//20.06.2025, 00:56
+
+namespace Tuple;
+
+public class StartUp
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        string fullName = input[0] + " " + input[1];
+        string address = input[2];
+
+        var firstTuple = new Tuple<string, string>(fullName, address);
+
+        Console.WriteLine(firstTuple);
+
+        input = Console.ReadLine().Split();
+        string name = input[0];
+        int beer = int.Parse(input[1]);
+
+        var secondTuple = new Tuple<string, int>(name, beer);
+
+        Console.WriteLine(secondTuple);
+
+        input = Console.ReadLine().Split();
+        int integer = int.Parse(input[0]);
+        double realNumber = double.Parse(input[1]);
+
+        var thirdTuple = new Tuple<int, double>(integer, realNumber);
+
+        Console.WriteLine(thirdTuple);
+    }
+}
