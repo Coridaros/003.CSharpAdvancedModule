a3a49095881d895f992f696293b00bc152309a2d
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-22 19:39:26 +0300

    add

---
+++ 016.GenericsExercise/002.GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,24 @@
+﻿//Generics - exercise, Task 002
+//19.06.2025, 15:22
+
+using GenericBoxOfInteger;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Box<int> box = new Box<int>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            box.Add(number);
+
+            Console.WriteLine(box.ToString());
+            box.Clear();
+        }
+    }
+}
