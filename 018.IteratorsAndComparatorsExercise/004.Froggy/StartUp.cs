e5cb20aa4efde2c7000649a23b537a8bb9e2703e
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-23 22:17:28 +0300

    add

---
+++ 018.IteratorsAndComparatorsExercise/004.Froggy/StartUp.cs
@@ -0,0 +1,17 @@
+﻿//Iterators and comparators - exercise, Task 004
+//20.06.2025, 10:42
+
+namespace Froggy;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        var lake = new Lake(Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList());
+
+        Console.WriteLine(String.Join(", ", lake));
+    }
+}
