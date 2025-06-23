59889388ce8478769b4b288c5de1cea121481623
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-23 22:18:19 +0300

    add

---
+++ 018.IteratorsAndComparatorsExercise/006.EqualityLogic/Person.cs
@@ -0,0 +1,37 @@
+﻿
+namespace EqualityLogic;
+
+public class Person : IComparable<Person>
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public int CompareTo(Person otherPerson)
+    {
+        if (Name.CompareTo(otherPerson.Name) != 0)
+        {
+            return Name.CompareTo(otherPerson.Name);
+        }
+        return Age.CompareTo(otherPerson.Age);
+    }
+
+    public override bool Equals(object obj)
+    {
+        Person otherPerson = obj as Person;
+
+        if (otherPerson == null)
+        {
+            return false;
+        }
+
+        return Name == otherPerson.Name && Age == otherPerson.Age;
+    }
+
+    public override int GetHashCode()
+    {
+        int hashCode = Name.GetHashCode() + Age.GetHashCode();
+
+        return hashCode;
+    }
+}
