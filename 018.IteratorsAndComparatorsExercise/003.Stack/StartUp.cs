1ce28b2058debc3d69a3e4e176feb8281d2af5ef
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-23 22:16:58 +0300

    add

---
+++ 018.IteratorsAndComparatorsExercise/003.Stack/StartUp.cs
@@ -0,0 +1,49 @@
+﻿//Iterators and comparators - exercise, Task 003
+//20.06.2025, 10:40
+
+namespace Stack;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Stack<string> stack = new Stack<string>();
+
+        string command;
+        while ((command = Console.ReadLine()) != "END")
+        {
+            string[] input = command.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            switch (input[0])
+            {
+                case "Push":
+                    string[] itemsToPush = input.Skip(1).ToArray();
+                    foreach (var item in itemsToPush)
+                    {
+                        stack.Push(item);
+                    }
+                    break;
+
+                case "Pop":
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch
+                    {
+                    }
+                    break;
+            }
+        }
+
+        foreach (var item in stack)
+        {
+            Console.WriteLine(item);
+        }
+
+        foreach (var item in stack)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
