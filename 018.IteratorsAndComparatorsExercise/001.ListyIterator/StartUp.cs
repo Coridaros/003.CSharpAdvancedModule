729f24d47a0107665d7c0a99dc196d7c02c92c53
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-23 22:15:34 +0300

    add

---
+++ 018.IteratorsAndComparatorsExercise/001.ListyIterator/StartUp.cs
@@ -0,0 +1,34 @@
+﻿//Iterators and comparators - exercise, Task 001
+//20.06.2025, 10:34
+
+namespace ListyIterator;
+
+public class StartUp
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().Split().Skip(1).ToList();
+
+        var listyIterator = new ListyIterator<string>(input);
+
+        string command = Console.ReadLine();
+
+        while (command != "END")
+        {
+            switch (command)
+            {
+                case "Move":
+                    Console.WriteLine(listyIterator.Move());
+                    break;
+                case "HasNext":
+                    Console.WriteLine(listyIterator.HasNext());
+                    break;
+                case "Print":
+                    Console.WriteLine(listyIterator.Print());
+                    break;
+            }
+
+            command = Console.ReadLine();
+        }
+    }
+}
