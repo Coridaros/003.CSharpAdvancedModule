14ffad4a252e298ad3169e07bb51b7f1eed69714
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-24 19:34:33 +0300

    update README.md

---
+++ 019.ExamPreparation/003.ComputerArchitecture/Computer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerArchitecture;
+
+public class Computer
+{
+    private string model;
+    private int capacity;
+    private List<CPU> multiprocessor;
+
+    public string Model { get; set; }
+    public int Capacity { get; set; }
+    public List<CPU> Multiprocessor => this.multiprocessor;
+    public int Count => this.multiprocessor.Count;
+
+    public Computer(string model, int capacity)
+    {
+        this.Model = model;
+        this.Capacity = capacity;
+        this.multiprocessor = new List<CPU>();
+    }
+
+    public void Add(CPU cpu)
+    {
+        if (this.Count < this.Capacity)
+        {
+            this.multiprocessor.Add(cpu);
+        }
+    }
+
+    public bool Remove(string brand)
+    {
+        var cpuToRemove = this.multiprocessor.FirstOrDefault(x => x.Brand == brand);
+        if (cpuToRemove != default)
+        {
+            this.multiprocessor.Remove(cpuToRemove);
+            return true;
+        }
+        return false;
+    }
+
+    public CPU MostPowerful() => this.multiprocessor
+            .OrderByDescending(x => x.Frequency)
+            .FirstOrDefault();
+
+    public CPU GetCPU(string brand) => this.multiprocessor.FirstOrDefault(x => x.Brand == brand);
+
+    public string Report()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine($"CPUs in the Computer {this.Model}:");
+
+        foreach (var item in this.multiprocessor)
+        {
+            sb.AppendLine($"{item.ToString()}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# 003.CSharpAdvancedModule
+# <p align="center" >C# Advanced Module @ SoftUni BG </p>
+
+## <a href="https://softuni.bg/certificates/details/245165/9409c910"> <p align="center"> CERTIFICATE </p> </a>
